ffcd4869e119b746eb701633341b91eadab3244d
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-11-01 21:53:32 +0200

    Adds markup for the list of countries

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,13 @@
 import countryCardTpl from "./templates/country.hbs"
+import countryListTpl from "./templates/flag-and-name.hbs"
 import debounce from "lodash.debounce"
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
 const refs = {
 inputCountry: document.querySelector('[id = search-box]'), 
-countryInfo: document.querySelector('.country-info'),
+  countryInfo: document.querySelector('.country-info'),
+ countryList: document.querySelector('.country-list')
 }
 
 const onInputValue = (e) => {
@@ -25,12 +27,22 @@ const onInputValue = (e) => {
         Notify.info('Too many matches found. Please enter a more specific name.');
           refs.countryInfo.innerHTML = ''
         return
-        }
-     const markup =countryCardTpl(country)
-    refs.countryInfo.innerHTML = markup
-    })
+      }
+      if (country.length === 1) {
+        const markup = countryCardTpl(country)
+        refs.countryInfo.innerHTML = markup
+        return
+      }
+     
+      if (country.status === 404) {
+        Notify.info('Oops, there is no country with that name.');
+        return
+      }
+      const markup = country.map(countryListTpl).join('')
+      refs.countryList.innerHTML = markup
+      })
     .catch(error => {
-      return Notify.failrue('Too many matches found. Please enter a more specific name.');
+      
     });
 
 }
