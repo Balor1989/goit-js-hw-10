8fbd19a8e83b7a2dde5731f63bcbbefab3b14220
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-11-01 21:09:29 +0200

    adds info then countries more than 10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,7 @@
 import countryCardTpl from "./templates/country.hbs"
 import debounce from "lodash.debounce"
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 const refs = {
 inputCountry: document.querySelector('[id = search-box]'), 
@@ -8,18 +10,27 @@ countryInfo: document.querySelector('.country-info'),
 
 const onInputValue = (e) => {
   let x = ''
-    x = e.target.value
-    console.log(x)
+  x = e.target.value
+  if (x === '') {
+    refs.countryInfo.innerHTML = ''
+    return
+  }
   fetch(`https://restcountries.com/v3.1/name/${x.trim()}?fields=name,capital,population,flags,languages`)
   .then(response => {
       return response.json()
     })
     .then(country => {
-     const markup = countryCardTpl(country)
+      if (country.length > 10)
+      {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+          refs.countryInfo.innerHTML = ''
+        return
+        }
+     const markup =countryCardTpl(country)
     refs.countryInfo.innerHTML = markup
     })
     .catch(error => {
-      return error
+      return Notify.failrue('Too many matches found. Please enter a more specific name.');
     });
 
 }
