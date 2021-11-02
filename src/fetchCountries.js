c5dfdd50301caf5795dc75cee139198bf953c982
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-11-02 10:36:59 +0200

    refactoring

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -6,60 +6,76 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
 inputCountry: document.querySelector('[id = search-box]'), 
-  countryInfo: document.querySelector('.country-info'),
- countryList: document.querySelector('.country-list')
+countryInfo: document.querySelector('.country-info'),
+countryList: document.querySelector('.country-list')
 }
 
 const onInputValue = (e) => {
   let x = ''
   x = e.target.value
   if (x === '') {
-    refs.countryInfo.innerHTML = ''
+    onCleanFindCountries()
     return
   }
+
   fetch(`https://restcountries.com/v3.1/name/${x.trim()}?fields=name,capital,population,flags,languages`)
+
   .then(response => {
       return response.json()
     })
+
     .then(country => {
+      
       if (country.length > 10)
       {
-        Notify.info('Too many matches found. Please enter a more specific name.');
-          refs.countryInfo.innerHTML = ''
-          refs.countryList.innerHTML=''
+        onManySuitable()
+        onCleanFindCountries()
         return
       }
+
       if (country.length === 1) {
-        refs.countryList.innerHTML=''
+        onCleanFindCountries()
         const markup = countryCardTpl(country)
         refs.countryInfo.innerHTML = markup
         return
       }
-     
+
       if (country.status === 404) {
-        refs.countryList.innerHTML=''
-        refs.countryInfo.innerHTML=''
-        Notify.failure('Oops, there is no country with that name.');
+        onCleanFindCountries()
+        onError()
         return
       }
-      refs.countryList.innerHTML=''
-      refs.countryInfo.innerHTML=''
+
+      onCleanFindCountries()
       const markup = country.map(countryListTpl).join('')
       refs.countryList.innerHTML = markup
       })
+
     .catch(error => {
-      Notify.failure('Oops, there is no country with that name.');
     });
 
 }
 
+function onFetchCountries() {
+  
+}
+
+function onManySuitable() {
+  Notify.info('Too many matches found. Please enter a more specific name.');
+}
+
+function onError(){
+  Notify.failure('Oops, there is no country with that name.');
+}
+
+function onCleanFindCountries(){
+  refs.countryList.innerHTML=''
+  refs.countryInfo.innerHTML=''
+}
 
 
+refs.inputCountry.addEventListener('input', debounce(onInputValue, 500))
 
-refs.inputCountry.addEventListener('input', debounce(onInputValue, 300))
-
-
-console.log(refs.inputCountry.currentTarget)
 
 
  
