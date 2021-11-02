c8dbfe3be752da07e8995e059d18c107210ee7eb
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-11-02 08:54:45 +0200

    Add a solution to the task. Need refactoring

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -26,23 +26,29 @@ const onInputValue = (e) => {
       {
         Notify.info('Too many matches found. Please enter a more specific name.');
           refs.countryInfo.innerHTML = ''
+          refs.countryList.innerHTML=''
         return
       }
       if (country.length === 1) {
+        refs.countryList.innerHTML=''
         const markup = countryCardTpl(country)
         refs.countryInfo.innerHTML = markup
         return
       }
      
       if (country.status === 404) {
-        Notify.info('Oops, there is no country with that name.');
+        refs.countryList.innerHTML=''
+        refs.countryInfo.innerHTML=''
+        Notify.failure('Oops, there is no country with that name.');
         return
       }
+      refs.countryList.innerHTML=''
+      refs.countryInfo.innerHTML=''
       const markup = country.map(countryListTpl).join('')
       refs.countryList.innerHTML = markup
       })
     .catch(error => {
-      
+      Notify.failure('Oops, there is no country with that name.');
     });
 
 }
@@ -50,7 +56,7 @@ const onInputValue = (e) => {
 
 
 
-refs.inputCountry.addEventListener('input', debounce(onInputValue, 500))
+refs.inputCountry.addEventListener('input', debounce(onInputValue, 300))
 
 
 console.log(refs.inputCountry.currentTarget)
